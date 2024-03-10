020cfe4c8af05a24d100b96bb2021a7cd7cef8ee
Bharath <bshet768@gmail.com>
2024-03-10 16:57:25 +0530

    Initilized express app middlewares updated utlis for stanadardize api error and response handling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import 'dotenv/config'
 import DB_CONNECT from "./db/index.js";
+import { app } from './app.js';
 
 DB_CONNECT()
+    .then(() => {
+        app.on('error', (err) => {
+            console.log("Error Connecting to Express APP". err)
+        })
+        app.listen(process.env.PORT || 8080, () => {
+            console.log(`Server is running at port ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("ERROR Connecting To DB", err)
+        process.exit(1)
+    })
